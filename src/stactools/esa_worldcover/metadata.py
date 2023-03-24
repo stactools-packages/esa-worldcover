506058ba24e191a14fcf1308dab8ed2e42830667
Preston Hartzell <preston.hartzell@gmail.com>
2023-03-24 10:57:09 -0400

    Subclass RasterFootprint to reduce memory requirements (#11)
    Reduces memory consumption in the mask creation step.

--- src/stactools/esa_worldcover/metadata.py
+++ src/stactools/esa_worldcover/metadata.py
@@ -2,12 +2,17 @@ import os
 from datetime import datetime
 from typing import Any, Dict, Optional
 
+import numpy as np
+import numpy.typing as npt
 import rasterio
 from pystac import Asset, MediaType
 from pystac.extensions.projection import AssetProjectionExtension
 from pystac.utils import make_absolute_href, str_to_datetime
-from shapely.geometry import box, mapping
+from shapely.geometry import box, mapping, shape
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.polygon import Polygon
 from stactools.core.io import ReadHrefModifier
+from stactools.core.utils.raster_footprint import RasterFootprint
 
 from stactools.esa_worldcover.constants import ASSET_PROPS
 
@@ -84,3 +89,34 @@ class Metadata:
     def quality_href(map_href: str) -> str:
         base_href = "_".join(map_href.split("_")[:-1])
         return f"{base_href}_InputQuality.tif"
+
+
+class ESAWorldCoverFootprint(RasterFootprint):
+    """ESA WorldCover tiles are large (36000x36000 pixels), causing the numpy-
+    based mask computation to consume >30GB for some tiles. This is more than
+    many machines support. This subclass avoids mask computation by assuming
+    the data array used to initialize the class is a rasterio mask.
+    """
+
+    def data_mask(self) -> npt.NDArray[np.uint8]:
+        # We will use a rasterio mask as the data array in the initializer, so
+        # no mask computation is required
+        return self.data_array
+
+    def data_extent(self, mask: npt.NDArray[np.uint8]) -> Optional[Polygon]:
+        data_polygons = [
+            shape(polygon_dict)
+            for polygon_dict, region_value in rasterio.features.shapes(
+                mask, transform=self.transform
+            )
+            if region_value == 255  # rasterio masks use 255 for True (not 1)
+        ]
+
+        if not data_polygons:
+            return None
+        elif len(data_polygons) == 1:
+            polygon = data_polygons[0]
+        else:
+            polygon = MultiPolygon(data_polygons).convex_hull
+
+        return polygon
